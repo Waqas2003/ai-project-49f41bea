648e4d6eb1a686484dc63c182e5a0a7a08e54fe1
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:12:11 +0500

    Add src\App.js

---
+++ src\App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Argentina Football Team</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Argentina_national_football_team_badge.svg/1200px-Argentina_national_football_team_badge.svg.png" alt="Argentina Football Team Logo" />
+      <p>La Albiceleste</p>
+    </div>
+  );
+}
+
+export default App;
